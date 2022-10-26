a1b47a110b054f3e474c96218a5390af801b7e35
fazilfazil1 <fazilfazil1>
2022-10-26 14:13:19 +0530

    Added Uploads component and css file

---
+++ src/components/Uploads/Uploads.tsx
@@ -0,0 +1,29 @@
+import s from './Uploads.module.css'
+
+// Upload buttons clicks
+const uploadbtn = () =>{
+    alert('Button is clicked')
+}
+
+
+const Uploads = () => {
+    return (
+        <div>
+            <div className={s.btncontainer}>
+               <button className={s.uploadbtn} onClick={uploadbtn}>NEW UPLOAD</button>
+            </div>
+           <div className={s.uploadcontainer}>
+             <h1 className={s.uploadtitle}>UPLOADS</h1>
+             <ul className={s.list}>
+                <li>Images</li>
+                <li>Videos</li>
+                <li>Press pages</li>
+                <li>Pdf ,Docs</li>
+            </ul>
+           </div>
+        </div>
+    )
+}
+
+export default Uploads
+
