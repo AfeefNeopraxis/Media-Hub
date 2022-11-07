a5b4fef570dde5bfdcd330a70caf8277afe644b2
fazilfazil1 <fazilfazil1>
2022-11-07 10:55:32 +0530

    pages createview aligned checkbox

--- src/components/Pages/Createpage.tsx
+++ src/components/Pages/Createpage.tsx
@@ -11,53 +11,9 @@ const Pages = () => {
                 </label>
                 <label className={p.inputlabel}>Description</label>
                 <textarea rows={8} className={p.textarea} placeholder="Describe your page here,these will shown for your users while they are browsing these page......"></textarea>
-                {/* <div className='flex justify-between'>
-                    <h2 className='font-bold text-sm'>Content type</h2>
-                    <div className="flex items-center mr-4 mb-2.5">
-                        <input type="checkbox" className="w-4 h-4" />
-                        <label className="ml-2 text-sm font-medium  dark:text-slate-700">Video</label>
-                    </div>
-                    <div>
-                        <input type="checkbox" className="w-4 h-4 bg-gray-100 rounded border-gray-300   dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                        <label className="ml-2 text-sm font-medium  dark:text-slate-700">Image</label>
-                    </div>
-                    <div>
-                        <input type="checkbox" className="w-4 h-4 bg-gray-100 rounded border-gray-300   dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                        <label className="ml-2 text-sm font-medium  dark:text-slate-700">Docs</label>
-                    </div>
-                </div> */}
-                {/* <div className='flex justify-between mb-2.5'>
-                    <h2 className='font-bold text-sm'>Choose who can upload</h2>
-                    <div className="flex items-center mr-4">
-                        <input type="checkbox" className="w-4 h-4" />
-                        <label className="ml-2 text-sm font-medium  dark:text-slate-700">My users</label>
-                    </div>
-                    <div>
-                        <input type="checkbox" className="w-4 h-4 bg-gray-100 rounded border-gray-300   dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                        <label className="ml-2 text-sm font-medium  dark:text-slate-700">Only me</label>
-                    </div>
-                    <div>
-                        <input disabled type="checkbox" className="w-4 h-4  bg-gray-100 rounded border-gray-300   dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                        <label className="ml-2 text-sm font-medium  dark:text-slate-700">My staff</label>
-                    </div>
-                </div> */}
-                {/* <div className='flex justify-between mb-2.5'>
-                    <h2 className='font-bold text-sm'>Choose template</h2>
-                    <div className="flex items-center mr-4">
-                        <input checked type="checkbox" className="w-4 h-4" />
-                        <label className="ml-2 text-sm font-medium  dark:text-slate-700">Default</label>
-                    </div>
-                    <div>
-                        <input disabled type="checkbox" className="w-4 h-4 bg-gray-100 rounded border-gray-300   dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                        <label className="ml-2 text-sm font-medium  dark:text-slate-700">Template 1</label>
-                    </div>
-                    <div>
-                        <input disabled type="checkbox" className="w-4 h-4  bg-gray-100 rounded border-gray-300   dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                        <label className="ml-2 text-sm font-medium  dark:text-slate-700">Template 2</label>
-                    </div>
-                </div> */}
+
                 <div>
-                    <table className="table-fixed">
+                    <table className="table-fixed w-full">
                         <tbody>
                                 <tr>
                                     <td><h2 className='font-bold text-sm'>Content type</h2></td>
@@ -97,8 +53,8 @@ const Pages = () => {
                         </tbody>
                     </table>
                 </div>
-                <button className={s.uploadbtn}>Save page</button>
             </form>
+            <button className={s.uploadbtn}>Save page</button>
         </div>
     )
 }
