c12b9858b7ccd2382089ab6b1e1cdd3d4c336941
fazilfazil1 <98819911+fazilfazil1@users.noreply.github.com>
2022-11-07 12:45:50 +0530

    create or edit page view compted
    squash and merged multiple commits from fazil to one

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -1,17 +1,14 @@
 import HeadSection from '../HeadSection'
 import s from './Body.module.css'
 import img from '../../images/commingsoon.png'
+import Pages from '../Pages/Createpage'
 
 const Body = ({ children }: any) => {
     return (
         <div className={s.rootContainer}>
-            {/* <HeadSection></HeadSection> */}
-            {/* <hr></hr> */}
-            {/* <div className='bg-red-500 w-1/2 h-1/2 overflow-hidden'> */}
-                {/* <p>This is body</p> */}
-                <img src={img}  className='h-full aspect-video m-auto overflow-hidden object-cover' />
-
-            {/* </div> */}
+            <HeadSection></HeadSection>
+            <hr></hr>
+            <Pages />
         </div>
     )
 }
---
+++ src/components/Uploads/UploadsBody.tsx
@@ -0,0 +1,14 @@
+import HeadSection from "../HeadSection"
+
+const UploadsBody = () => {
+    return (
+        <div>
+            <HeadSection></HeadSection>
+            <hr></hr>
+            <div>Uploads Body here...</div>
+        </div>
+    )
+}
+
+export default UploadsBody
+
