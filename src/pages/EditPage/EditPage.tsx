3b3c20b7c9392f0e3cca77531f6b9135fd1d7bd9
Afeef K K <afeef.abdulla@neopraxis.in>
2023-01-21 16:16:27 +0530

    Removed MultipleSelection for better one

--- src/pages/EditPage/EditPage.tsx
+++ src/pages/EditPage/EditPage.tsx
@@ -1,81 +1,30 @@
 import { useContext } from "react";
-import InputText, {
-  InputCheckbox,
-  InputTextArea
-} from "../../components/Common/FormInputs";
+import InputText, { InputTextArea } from "../../components/Common/FormInputs";
 import { AppContext } from "../../helpers/context/appContext";
 import p from "./EditPage.module.css";
-
-type MultipleSelection = {
-  label: string;
-  options: Options[];
-};
-
-type Options = {
-  name: string;
-  disabled?: boolean;
-};
 
 const Pages = () => {
   const { currentPage } = useContext(AppContext);
 
-  var choices: MultipleSelection[] = [
-    {
-      label: "Content type",
-      options: [{ name: "Video" }, { name: "Image" }, { name: "Docs" }],
-    },
-    {
-      label: "Choose who can upload",
-      options: [
-        { name: "My users" },
-        { name: "Only me" },
-        { name: "My staff", disabled: true },
-      ],
-    },
-    {
-      label: "Choose template",
-      options: [
-        { name: "Default" },
-        { name: "Template 1", disabled: true },
-        { name: "Template 2", disabled: true },
-      ],
-    },
-  ];
-
   return (
     <div className={p.container}>
-      <form>
-        <InputText label="Title" value={currentPage?.name} />
+      <form onSubmit={(e) => {
+        e.preventDefault();
+      }}>
+        <InputText
+          label="Title"
+          value={currentPage?.name} />
         <InputTextArea
           label="Description"
           placeholder="Describe your page here,these will shown for your users while they are browsing these page......"
           value={currentPage?.description}
         />
-        <div>
-          <table className="table-fixed w-full h-36">
-            <tbody>
-              {choices.map((choice, choiceIndex) => (
-                <tr key={"choice-" + choiceIndex}>
-                  <td>
-                    <h2 className="font-bold text-sm">{choice.label}</h2>
-                  </td>
-                  {choice.options.map((option, optionIndex) => (
-                    <td key={"option-" + optionIndex}>
-                      <InputCheckbox
-                        label={option.name}
-                        disabled={option.disabled}
-                      />
-                    </td>
-                  ))}
-                </tr>
-              ))}
-            </tbody>
-          </table>
+        <div className="grid gap-2 grid-cols-1 md:grid-cols-2">
         </div>
-      </form>
-      <button className="bg-amber-400 font-medium mt-5 py-2 px-4 rounded hover:bg-amber-500">
+        <button className="bg-amber-400 font-medium mt-5 py-2 px-4 rounded hover:bg-amber-500" >
         Save page
       </button>
+      </form>
     </div>
   );
 };
